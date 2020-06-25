8399d0d76737602b05efde8dd882b1a50da28069
Maxim Loginov <zeliboba@mail.ru>
2020-06-25 13:20:55 +0700

    add command line options with default values

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
 import glob
 import os
+import argparse
+
 
 from spotit.utilities import generate_images
 from spotit import create_sheets
 
 
-filename = 'cards.pdf'  # filename of the PDF
-order = 5  # number of images at each card
-images_path = './images'  # path where to store generated images
+# parse command line argumnents
+parser = argparse.ArgumentParser(description="""
+Plots the card for the Dobble or Spot It! game.  More about the game see
+http://en.wikipedia.org/wiki/Dobble
+""")
+parser = argparse.ArgumentParser(
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter)  # add default values to help
+parser.add_argument('-i', '--images', default='images', type=str,
+                    help="Input image directory, for 5 images per card 31 images rquired")
+parser.add_argument('-o', '--output', default='cards.pdf', type=str,
+                    help="Output file name")
+parser.add_argument('-O', '--order', default=5,type=int,
+                    help="Number of images per card, i.e. order + 1")
+args = parser.parse_args()
+
+filename = args.output  # filename of the PDF
+order = args.order  # number of images at each card
+images_path = args.images # path where to store generated images
 
 # create a directory for generated images
 if not os.path.isdir(images_path):
